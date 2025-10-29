1e6573268d7db78b431a2cd4c34b6e4222624160
nliubeznikov <n.lyubeznikov@uzum.com>
2025-10-30 01:19:59 +0500

    Complete task CF

--- src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/minPrice/PriceAggregator.java
@@ -1,7 +1,14 @@
 package course.concurrency.m2_async.minPrice;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class PriceAggregator {
 
@@ -13,12 +20,29 @@ public class PriceAggregator {
 
     private Collection<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l, 67l, 123l, 768l);
 
+    public static final Long WAIT_TIME_LIMIT_MS = 2900L;
+
     public void setShops(Collection<Long> shopIds) {
         this.shopIds = shopIds;
     }
 
     public double getMinPrice(long itemId) {
-        // place for your code
-        return 0;
+        ExecutorService executor = Executors.newFixedThreadPool(shopIds.size());
+        try {
+            List<CompletableFuture<Double>> responseCFList = shopIds.stream().
+                    map(id -> CompletableFuture.supplyAsync(
+                            () -> priceRetriever.getPrice(itemId, id), executor)
+                            .orTimeout(WAIT_TIME_LIMIT_MS, TimeUnit.MILLISECONDS)
+                            .exceptionally(e -> Double.NaN)
+                    ).toList();
+
+            return responseCFList.stream()
+                    .map(CompletableFuture::join)
+                    .filter(v -> !Double.isNaN(v))
+                    .min(Double::compareTo) // Ищем минимальное значение
+                    .orElse(Double.NaN);
+        } finally {
+            executor.shutdownNow();
+        }
     }
 }
